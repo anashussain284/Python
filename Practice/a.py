73b12bfa8b7567662f9a723ee324b87de7552858
Anas Hussain M <anashussain284@gmail.com>
2023-12-27 13:04:53 +0530

    Create Practice directory

---
+++ Practice/a.py
@@ -0,0 +1,52 @@
+# print(type(float(1)))
+
+# print(type(float(1)))
+
+# a = 1;
+# A = "1";
+
+# print(a, type(a), id(a))
+# print(A, type(A), id(A))
+
+
+# a = "Hello world";
+# print(a[-11])
+
+# for x in "Banana":
+#     print(x)
+
+# a = "Anas";
+# print(a, type(a), len(a))
+
+
+# txt = "The best things in life are free"
+# key = "free"
+# # print(txt)
+# # print(key)
+# # found = key in txt
+# # print(found)
+
+# if key not in txt:
+#     # print("Yes", key, "is present in", txt)
+#     print("No", key, "is not present in", txt)
+# else:
+#     # print("No", key, "is not present in", txt)
+#     print("Yes", key, "is present in", txt)
+
+
+
+# print()
+# print()
+# print()
+# print()
+# print()
+
+# if "super" in "Anas is super":
+#     print(True)
+# else:
+#     print(False)
+
+
+a = "Hello world"
+print(a)
+print(a[2:5])
